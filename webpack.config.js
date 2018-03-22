28688eba9e5f40f821797078679960f8bf254a0f
Patricio de la Guardia <padlgt01@gmail.com>
2018-03-22 16:31:58 -0400

    optimize webpack performance for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+// webpack.optimization.splitChunks.chunks = 'all';
 
 module.exports = {
   entry: ["./source/js/app.js", "./source/css/styles.css"],
   output: {
     path: path.resolve(__dirname, './source/dist'),
-    filename: 'dist.js'
+		filename: 'dist.js',
+		chunkFilename: 'commons.js'
   },
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   module: {
     rules: [  
-      {test: /\.js$/, use: 'babel-loader'},
-      {test: /\.css$/, use: ['style-loader', 'css-loader']}
+			{test: /\.js$/, include: [path.resolve(__dirname, 'source/components'), path.resolve(__dirname, 'source/js')], use: 'babel-loader'},
+      {test: /\.css$/, include: path.resolve(__dirname, 'source/css'), use: ['style-loader', 'css-loader']}
     ]
-  },
+	},
+	mode: "development",
   plugins: [
-    new CleanWebpackPlugin('./source/dist')
-  ],
+		new CleanWebpackPlugin('./source/dist'),
+	],
   devServer: {
     contentBase: path.join(__dirname, "./source/"),
     compress: true,
