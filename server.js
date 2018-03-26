d5b9a3992c74d1217e3029665b51032b1551e5d4
Patricio de la Guardia <padlgt01@gmail.com>
2018-03-26 13:42:32 -0400

    Add more specific route handling to server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static("./source"));
 
 const PORT = process.env.PORT || 4000;
 
-app.get("*", function(req, res) {
+app.get(['/', '/about', '/contact'], function(req, res) {
   res.sendFile(path.resolve(__dirname, "./source/index.html"));
 });
 
