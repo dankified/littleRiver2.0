5eadcca910053f2bd659a2e69cfcef65ac906411
Patricio de la Guardia <padlgt01@gmail.com>
2018-03-26 11:20:40 -0400

    Resolve all requests with same handler

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var express = require('express');
+var express = require("express");
+var path = require("path");
 var app = express();
 
-app.use(express.static('./source'));
+app.use(express.static("./source"));
 
 const PORT = process.env.PORT || 4000;
 
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "./source/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("Server listening on port", PORT);
-})
+});
