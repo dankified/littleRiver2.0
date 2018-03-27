32780e2b675f6fe2d75f194e0549ef5ee83ec471
Patricio de la Guardia <padlgt01@gmail.com>
2018-03-27 15:12:29 -0400

    Add proxy webpack build for proxy configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 // webpack.optimization.splitChunks.chunks = 'all';
 
 module.exports = {
   entry: ["./source/js/app.js", "./source/css/styles.scss"],
   output: {
-    path: path.resolve(__dirname, './source'),
-		filename: 'dist.js',
-		chunkFilename: 'commons.js'
+    path: path.resolve(__dirname, "./source"),
+    filename: "dist.js",
+    chunkFilename: "commons.js"
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
-    rules: [  
-			{test: /\.js$/, include: [path.resolve(__dirname, 'source/components'), path.resolve(__dirname, 'source/js')], use: 'babel-loader'},
-      {test: /\.scss$/, include: path.resolve(__dirname, 'source/css'), use: ['style-loader', 'css-loader', 'sass-loader']}
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "source/components"),
+          path.resolve(__dirname, "source/js")
+        ],
+        use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, "source/css"),
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
     ]
-	},
-	mode: "development",
-  plugins: [
-		new CleanWebpackPlugin('./source/dist.js'),
-	],
+  },
+  mode: "development",
+  plugins: [new CleanWebpackPlugin("./source/dist.js")],
   devServer: {
     contentBase: path.join(__dirname, "./source/"),
     compress: true,
+    proxy: { "*": "http://localhost:4000" },
     port: 3000
   }
-}
+};
