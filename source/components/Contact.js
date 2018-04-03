e667ff07bdf6624b4edb81298034bff95dc23a23
Patricio de la Guardia <padlgt01@gmail.com>
2018-04-03 12:42:49 -0400

    Add contact form styling

--- source/components/Contact.js
+++ source/components/Contact.js
@@ -1,16 +1,57 @@
 import React, { Component } from "react";
+import {Button, Form, Dropdown} from 'semantic-ui-react';
+
+var options = [
+  {key: 'Phone', value: 'Phone', text: 'Phone'}, 
+  {key: 'Email', value: 'Email', text: 'Email'},
+  {key: 'Other', value: 'Other', text: 'Other'}
+];
+
+function contactOptionChange(ev, data) {
+  console.log("Event", ev);
+  console.log("Data", data);
+  if(data.value === "Other") {
+    this.setState({
+      otherDisabled: false
+    })
+  } else {
+    this.setState({
+      otherDisabled: true
+    })
+  }
+}
 
 export default class Contact extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sending: false,
+      otherDisabled: true
+    }
+    this.contactOptionChange = contactOptionChange.bind(this);
   }
 
   render() {
     return (
       <div id="mail-form-div">
-        <form method="POST" action="send" id="mail-form">
-          <input type="submit" value="Send email" />
-        </form>
+        <h2>Contact</h2>
+        <p>Little River Friesians, 150 Shady Rest Road, Havana, Florida 32333 | Phone: (305) 476-5151</p>
+        <Form>
+          <Form.Input label="Full Name" placeholder="Full Name" type="text" />
+          <Form.Group widths="equal">
+            <Form.Input fluid label="Email" placeholder="Email" />
+            <Form.Input fluid label="Phone" placeholder="Phone" />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <Form.Field>
+              <label>How would you like to be contacted?</label>
+              <Dropdown placeholder="Select Contact Method" search={false} selection options={options} onChange={this.contactOptionChange}/>
+            </Form.Field>
+            <Form.Input fluid label="Other Contact Option" placeholder="Other" disabled={this.state.otherDisabled}/>
+          </Form.Group>
+          <Form.TextArea label="Message" placeholder="Message" />
+          <Button type="submit">Submit</Button>
+        </Form>
       </div>
     );
   }
